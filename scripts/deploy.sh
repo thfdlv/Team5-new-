cbc00dcd46831b977c5f8da1e3883e03464a25f4
thfdlv <223carpediem@naver.com>
2025-04-02 14:07:00 +0900

    수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 echo "✅ 배포 시작"
 
-# 기존 WAR 삭제 (선택)
+# 기존 WAR 삭제
 rm -f /opt/tomcat/webapps/*.war
 
-# S3에서 최신 WAR 다운로드 (버킷 이름은 너희 팀의 실제 이름으로 바꿔줘)
-aws s3 cp s3://app-deploy-jp/jsonExam-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/
+# 최신 WAR 다운로드 (이름과 상관없이 최신 1개)
+aws s3 cp s3://app-deploy-jp/*.war /opt/tomcat/webapps/
 
-# Tomcat 재시작 (systemctl 방식, Amazon Linux 2023 기준)
+# Tomcat 재시작
 sudo systemctl restart tomcat
 
 echo "✅ 배포 완료"
-
