4016f608239077b31d1b71586a1b229b1c1e758b
thfdlv <223carpediem@naver.com>
2025-04-02 14:52:46 +0900

    수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,10 @@ echo "✅ 배포 시작"
 # 기존 WAR 삭제
 rm -f /opt/tomcat/webapps/*.war
 
-# 최신 WAR 다운로드 (이름과 상관없이 최신 1개)
-aws s3 cp s3://app-deploy-jp/*.war /opt/tomcat/webapps/
+# 최신 WAR 1개만 추출해서 다운로드
+LATEST_WAR=$(aws s3 ls s3://app-deploy-jp/ | sort | tail -n 1 | awk '{print $4}')
+echo "📦 최신 WAR 파일: $LATEST_WAR"
+aws s3 cp s3://app-deploy-jp/$LATEST_WAR /opt/tomcat/webapps/
 
 # Tomcat 재시작
 sudo systemctl restart tomcat
