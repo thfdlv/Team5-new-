eecdb61348a284aab045cb728a5280fd12b3b689
thfdlv <223carpediem@naver.com>
2025-04-03 10:31:21 +0900

    beforeInstall 수정

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -4,8 +4,7 @@ set -e
 echo "[BeforeInstall] 앱 디렉토리 초기화 중..."
 
 if [ -d /home/ec2-user/app ]; then
-  rm -rf /home/ec2-user/app/*
+  sudo rm -rf /home/ec2-user/app/*
 fi
 
 echo "[BeforeInstall] 완료!"
-
